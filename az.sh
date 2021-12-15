aea6d82d31e477767a26ca7744d1f6f92a4904ac
anu <59210087+stfuanu@users.noreply.github.com>
2021-12-15 20:55:56 +0530

    Update az.sh

--- az.sh
+++ az.sh
@@ -22,13 +22,32 @@ az vm list -g MyResourceGroup --query "[].id" -o tsv
 
 az group create --name MyResourceGroup --location eastus
 
+# very elaborate tutorial
+# https://docs.microsoft.com/en-us/azure/virtual-machines/linux/tutorial-manage-vm
+
 az vm create -n MyVm -g MyResourceGroup --image UbuntuLTS
 
-# 
+# Available VM sizes in region
+az vm list-sizes --location eastus --output table | head -n 10
 az vm list-sizes -l eastus
+
+az vm create \
+    --resource-group myResourceGroupVM \
+    --name myVMname \
+    --image UbuntuLTS \
+    --size Standard_B1ls \
+    --generate-ssh-keys
+
 
 # Get IP address for a VM
 az vm list-ip-addresses -g MyResourceGroup -n MyVm
+# https://docs.microsoft.com/en-us/azure/virtual-network/ip-services/virtual-network-deploy-static-pip-arm-cli
+
+
+# Create FQDN (fully qualified domain name) for this VM (ip/server) 
+# https://docs.microsoft.com/en-us/azure/virtual-machines/create-fqdn
+# https://docs.microsoft.com/en-us/azure/virtual-machines/custom-domain
+
 
 ssh azureuser@<list-ip-addresses>
 
@@ -43,3 +62,26 @@ az vm list-vm-resize-options --ids $(az vm list -g MyResourceGroup --query "[].i
 
 # {Resize VM or dellocate(region)} 
 # https://docs.microsoft.com/en-us/azure/virtual-machines/resize-vm?tabs=cli
+
+# Delete a VM & a Resource Group 
+az vm delete -g MyResourceGroup -n MyVm --yes
+az group delete --name MyResourceGroup
+
+# Create VM from image
+# https://docs.microsoft.com/en-us/azure/virtual-machines/vm-generalized-image-version?tabs=cli
+
+# Create Images :
+# https://docs.microsoft.com/en-us/azure/virtual-machines/linux/tutorial-custom-images
+
+# Open/Manage ports for vm or setup (services-http/https etc..) ports , source/dest. 
+# https://docs.microsoft.com/en-us/azure/virtual-machines/windows/nsg-quickstart-portal
+
+
+# --------
+
+# Azure DNS : (https://docs.microsoft.com/en-us/azure/dns/)
+# https://docs.microsoft.com/en-us/cli/azure/network/dns/record-set?view=azure-cli-latest
+
+az network dns record-set cname create
+az network dns record-set cname list -g MyResourceGroup -z www.mysite.com
+
